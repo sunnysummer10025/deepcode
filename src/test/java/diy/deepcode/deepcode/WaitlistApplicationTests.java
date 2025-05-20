718ef8cb6e7e3caf1dae2b69629c283cc07c60e9
Samson <samson@angineers.com>
2025-05-20 10:17:04 -0700

    Initial commit

---
+++ src/test/java/diy/deepcode/deepcode/WaitlistApplicationTests.java
@@ -0,0 +1,13 @@
+package diy.deepcode.deepcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
